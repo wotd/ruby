d9b129f778c6cb25e67feb91dfc29763fb44880b
wotd <ngebicki@gmail.com>
2016-05-12 23:05:12 +0200

    Initial commit

---
+++ 00-name_generator.rb
@@ -0,0 +1,9 @@
+#ToDo: add files with dictionaries
+
+first_names = ["Anders","Mikael","Darth"]
+last_names = ["Maul", "Vader", "Sirius"]
+
+x = first_names.size
+y = last_names.size
+
+puts "Your new name is... #{first_names[rand(x)]} #{last_names[rand(y)]} "
